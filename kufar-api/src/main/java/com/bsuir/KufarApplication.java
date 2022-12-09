2ad98fa78987b693343d36654edb54b986f15fc8
Dima <dbuyvid@gmail.com>
2022-12-09 12:52:02 +0300

    Substituted directory for entry class (due to scope)

--- kufar-api/src/main/java/com/bsuir/KufarApplication.java
+++ kufar-api/src/main/java/com/bsuir/KufarApplication.java
@@ -1,8 +1,10 @@
-package com.bsuir.kufar;
+package com.bsuir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @SpringBootApplication
 public class KufarApplication {
 
